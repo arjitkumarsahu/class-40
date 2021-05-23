b1905494f3c27933976b8e220b493ceee46ff30d
arjitkumarsahu <arjit2006kumar@gmail.com>
2021-05-23 20:22:21 +0530

    modified

--- js/Game.js
+++ js/Game.js
@@ -44,7 +44,8 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
-    
+    player.getRank();
+
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
       image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
@@ -55,7 +56,7 @@ class Game {
       var index = 0;
 
       //x and y position of the cars
-      var x = 175 ;
+      var x = 30 ;
       var y;
 
       for(var plr in allPlayers){
@@ -93,6 +94,8 @@ class Game {
 
     if(player.distance > 3860){
       gameState = 2;
+      player.rank+=1;
+      Player.updateRank(player.rank);
     }
    
     drawSprites();
@@ -100,5 +103,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
